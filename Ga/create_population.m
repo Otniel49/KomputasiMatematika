9cbad6d47a0999f585d1b1137b734aec7e12d9b1
unknown <Otniel49.otnielntoy82@gmail.com>
2025-05-22 04:58:57 +0700

    Tugas 4_Otniel Anugrah Ntoy_662022013

--- Ga/create_population.m
+++ Ga/create_population.m
@@ -3,7 +3,7 @@ function population = create_population(target,population_size)
 population = struct.empty(population_size,0);
 for i=1:population_size
     genes = create_genes(length(target));
-    fitness = Calculate_fitness(genes,target);
+    fitness = Calculate_fitness(genes,starget);
     population(i).genes = genes;
     population(i).fitness = fitness;
 end
